cb0c4b0c6d3f1818ec3194ccaa933bc7dc43070a
k1k1k <72328573+k1k1k@users.noreply.github.com>
2020-10-04 10:52:04 +0700

    pemula cuk
    skip

---
+++ algoritma.py
@@ -0,0 +1,10 @@
+angka1 = input("masukkan angka pertama:")
+angka2 = input("masukkan angka kedua: ")
+if angka1 > angka2:
+	print("nilai terbesar: ", angka1)
+elif angka1 < angka2:
+	print("nilai terbesar: ", angka2)
+elif angka1 == angka2:
+	print("kedua angka sama aja cuk")
+else
+print("Lu salah masukin cuk kodenya")
